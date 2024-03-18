6370627364982d00e1cf1f9256fd6ea4455821ec
AadityaMunjal <aadityamunjal06@gmail.com>
2024-03-18 11:05:59 +0530

    submission routes

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,16 @@ app.use(
     type: ["application/json", "text/plain"],
   })
 );
+
 const assignmentRoutes = require("./routes/assignments");
 const questionRoutes = require("./routes/questions");
 const userRoutes = require("./routes/user");
+const submissionRoutes = require("./routes/submissions");
 
 app.use("/assignments", assignmentRoutes);
 app.use("/questions", questionRoutes);
 app.use("/user", userRoutes);
+app.use("/submissions", submissionRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
