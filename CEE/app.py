43ebac4e38c45fc504292ae38ef2d057990b7e3f
AadityaMunjal <aadityamunjal06@gmail.com>
2024-03-26 13:16:26 +0530

    functioned and unfunctioned testing

--- CEE/app.py
+++ CEE/app.py
@@ -24,41 +24,44 @@ def exec_python(c):
 
 
 app = Flask(__name__)
-# client = docker.from_env()
+
+func_validation = """
+funcs = ([f for f in locals().values() if type(f) == type(lambda x: None) and f.__name__[0] in "Qq"])
+
+using_funcs = len(funcs) != 0
+# print("using functions", using_funcs)
+"""
 
 
-# def stream_container_logs(container):
-#     for line in container.logs(stream=True):
-#         yield line.decode("utf-8")
+# static
+inp_handler = """
+input_idx = 0
+test_case_idx = 0
 
 
-# def execute_code(code):
-#     try:
-#         start = timer()
-#         container_name = f"code-execution-{uuid.uuid4()}"
+# replacing built in input function with custom input feeder to give in inputs from testcases
+def answer_inputs(_=""):
+    global input_idx, test_case_idx, testcases
+    tc_inputs = testcases["__inputs"][test_case_idx]
+    curr_input = tc_inputs[input_idx]
 
-#         container = client.containers.run(
-#             "python:3.9-slim",
-#             command=["python", "-c", code],
-#             name=container_name,
-#             detach=True,
-#             remove=True,
-#             network_disabled=True,
-#         )
+    if input_idx == len(testcases["__inputs"][test_case_idx]) - 1:
+        input_idx = 0
+        test_case_idx += 1
+    else:
+        input_idx += 1
 
-#         container_output = ""
+    return curr_input
 
-#         for log_line in stream_container_logs(container):
-#             container_output += log_line
 
-#         container.stop()
+input = answer_inputs
 
-#         end = timer()
-#         print(container_output)
-#         return container_output, end - start
+"""
 
-#     except Exception as e:
-#         return str(e)
+# static
+testing = """
+print(testcases["__outputs"])
+"""
 
 
 @app.route("/execute", methods=["GET"])
@@ -67,14 +70,49 @@ def handle_execute():
     try:
         data = request.get_json()
         code = data.get("c")
-        print(data)
+        raw_test_cases = data.get("tc")
+        code_count = 1 if "def " in code else 2  # make this dynamic later
 
-        start = timer()
-        result = exec_python(code)
-        end = timer()
+        if "def " not in code:
+            c = (
+                "testcases = "
+                + raw_test_cases
+                + inp_handler
+                + (code + "\n") * code_count
+            )
+            print(c)
+
+            start = timer()
+            result = exec_python(c)
+            print(result)
+            end = timer()
+        else:
+            c = (
+                inp_handler
+                + (code + "\n") * code_count
+                + "testcases = "
+                + raw_test_cases
+                + testing
+            )
+            print(c)
+
+            start = timer()
+            result = exec_python(c)
+            end = timer()
+
+        if "def " not in code:
+            checks = []
+            for i in range(len(eval(raw_test_cases)["__outputs"])):
+                o = result.split("\n")[i]
+                if str(o) != str(eval(raw_test_cases)["__outputs"][i]):
+                    checks.append(False)
+                else:
+                    checks.append(True)
+        else:
+            checks = eval(result)
 
         print(jsonify({"result": result}))
-        return jsonify({"result": result, "exec_time": end - start})
+        return jsonify({"checks": checks, "exec_time": end - start})
     except Exception as e:
         return jsonify({"error": str(e)})
 
--- CEE/ce_server.py
+++ CEE/ce_server.py
@@ -2,61 +2,34 @@ import subprocess
 import requests
 
 code = """
-a = int(input('enter int '))
-b = int(input('enter int ')) 
-if a > b:
-    print('a is greater')
-else:
-    print('b is greater')
-"""
-
-
-# get code (single qs at once only), testcases with correct outputs, replace inputs with test data, compare outputs with testcases, return passed test cases (one by one) and errors
-
-raw_test_cases = """[
-    {
-        "command": "",
-        "__inputs": ("5", "6"),
-        "__outputs": ("b is greater"),
-    },
-    {
-        "command": "",
-        "__inputs": ("-1", "-3"),
-        "__outputs": ("a is greater"),
-    }
-]
-"""
-
-inp_handler = """
-input_idx = 0
-test_case_idx = 0
-
-
-# replacing built in input function with custom input feeder to give in inputs from testcases
-def answer_inputs(_):
-    global input_idx, test_case_idx, testcases
-    tc_inputs = testcases[test_case_idx]["__inputs"]
-    curr_input = tc_inputs[input_idx]
-
-    if input_idx == len(testcases[test_case_idx]["__inputs"]) - 1:
-        input_idx = 0
-        test_case_idx += 1
+def q1(a, b):
+    if a > b:
+        return a
     else:
-        input_idx += 1
-
-    return curr_input
-
-
-input = answer_inputs
-
+        return b
 """
-c = "testcases = " + raw_test_cases + inp_handler + (code * 2)
-print(c)
+
+code = """
+a = int(input())
+b = int(input())
+if a > b:
+    print(a)
+else:
+    print(b)
+"""
+
+raw_test_cases = """{
+    "__inputs": tuple(()),
+    "__outputs": tuple((q1(5, 6) == 6, q1(-1, -3) == -1)),
+}"""
+
+raw_test_cases = """{
+    "__inputs": tuple(((5, 6), (-1, -3))),
+    "__outputs": tuple((6, -1)),
+}"""
 
 
-def execute_test_cases():
-    res = requests.get("http://127.0.0.1:5000/execute", json={"c": c}).json()
-    print(res)
-
-
-execute_test_cases()
+res = requests.get(
+    "http://127.0.0.1:5000/execute", json={"c": code, "tc": raw_test_cases}
+).json()
+print(res)
