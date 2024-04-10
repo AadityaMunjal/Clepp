2c24501b7255a9ec48de53f545c44af3aa75b175
AadityaMunjal <aadityamunjal06@gmail.com>
2024-04-10 14:30:47 +0530

    fix(web): revise admin dashboard ui

--- web/src/pages/admin/AdminHome.tsx
+++ web/src/pages/admin/AdminHome.tsx
@@ -59,7 +59,7 @@ const AssignmentCard: React.FC<AssignmentCardProps> = ({ assignment }) => {
       onClick={() => {
         navigate(`/admin/assignment/${assignment.id}`);
       }}
-      className="bg-zinc-700 min-h-36 ml-10 mr-10 mt-20 rounded-lg p-4 cursor-pointer"
+      className="bg-zinc-800 min-h-36 ml-10 mr-10 mt-20 rounded-lg p-4 cursor-pointer"
     >
       <div className="flex justify-between">
         <div>
@@ -72,13 +72,13 @@ const AssignmentCard: React.FC<AssignmentCardProps> = ({ assignment }) => {
           </div>
         </div>
 
-        <div className="bg-blue-500 rounded-lg h-8 text-center p-1">
+        <div className="bg-zinc-600 rounded-lg h-8 text-center p-1">
           <span className="font-semibold">DUE: </span>
           {readableDate}
         </div>
       </div>
 
-      <div className="w-full bg-gray-200 rounded-full h-2.5 mt-12">
+      <div className="w-full bg-zinc-200 rounded-full h-2.5 mt-12">
         <div
           className="bg-green-500 h-2.5 rounded-full transition-all duration-300 ease-out"
           style={{ width: `${percentage}%` }}
@@ -90,7 +90,7 @@ const AssignmentCard: React.FC<AssignmentCardProps> = ({ assignment }) => {
   );
 };
 
-export default function UserDashboard() {
+export default function AdminDashboard() {
   const [error, setError] = useState("");
   const { logout } = useAuth();
 
@@ -111,15 +111,51 @@ export default function UserDashboard() {
 
   return (
     <>
-      <div className="min-h-screen text-white bg-zinc-800">
-        <h2 className="text-center mb-4">ADMIN DASHBOARD</h2>
-        <nav>
-          <Link to="/admin/create">Create assignment</Link>
-          <Link to="/admin/add-users">Add users</Link>
-          <button onClick={handleLogout}>Log Out</button>
+      <div className="min-h-screen text-white bg-zinc-900">
+        <nav className="bg-zinc-800 fixed w-full z-20 top-0 start-0 border-b border-zinc-600">
+          <div className="w-screen mx-auto p-4">
+            <div className="w-auto order-1">
+              <ul className="flex justify-center p-0 font-medium rounded-lg space-x-8 mt-0 border-0 bg-zinc-800 border-zinc-700">
+                <li>
+                  <a
+                    href="/admin"
+                    className="block py-2 px-3 hover:bg-zinc-700 rounded bg-transparent p-0 text-white"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/admin/create"
+                    className="block py-2 px-3 rounded p-0 text-white hover:bg-zinc-700 hover:text-white hover:bg-transparent border-zinc-700"
+                  >
+                    Create Assignment
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/admin/add-users"
+                    className="block py-2 px-3 rounded p-0 text-white hover:bg-zinc-700 hover:text-white hover:bg-transparent border-zinc-700"
+                  >
+                    Add Students
+                  </a>
+                </li>
+                <li>
+                  <a
+                    onClick={handleLogout}
+                    className="block py-2 px-3 rounded p-0 text-white hover:bg-zinc-700 hover:text-white hover:bg-transparent border-zinc-700"
+                  >
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
+        <div className="my-20"> </div>
         <div>
-          <div>Active assignments</div>
+          <div className="ml-10 mt-24 text-2xl">Active assignments</div>
           <div className="grid grid-cols-3">
             {assignments &&
               assignments.map((a: Assignment) => {
