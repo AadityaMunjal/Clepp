aaccba071d141e8c218390e938644e0f13467fe2
AadityaMunjal <aadityamunjal06@gmail.com>
2024-04-24 15:15:51 +0530

    feat(cee): update docs with all types of validation

--- cee/ce_server.py
+++ cee/ce_server.py
@@ -1,40 +1,30 @@
 import requests
 
-# code = """
-# def q1(a, b):
-#     if a > b:
-#         return a
-#     else:
-#         return b
-# """
-
-# code = """
-# a = int(input())
-# b = int(input())
-# if a > b:
-#     print(a)
-# else:
-#     print(b)
-# """
-
-# raw_test_cases = """{
-#     "__inputs": tuple(()),
-#     "__outputs": tuple((q1(5, 6) == 6, q1(-1, -3) == -1)),
-# }"""
-
-# raw_test_cases = """{
-#     "__inputs": tuple(((5, 6), (-1, -3))),
-#     "__outputs": tuple((6, -1)),
-# }"""
-
-new_version = """{
-        "__inputs": ((5, 6), (-3, -1)),
-        "__validate": "(q1() == 6, q1() == -1)",
-        "__outputs": (6, -1),
-    }"""
-
 # validate must be a code block that gets executed and its print output be the bool testing value
 # with inputs but not returning
+new_version = {
+    "exec_count": 2,
+    "__inputs": tuple(((5, 6), (-3, -1))),
+    "__validate": """
+q1()
+q1()
+""",
+    "__outputs": tuple((6, -1)),
+}
+
+code = """
+def q1():
+    a = int(input())
+    b = int(input())
+    if a > b:
+        print(a)
+        # return a
+    else:
+        print(b)
+        # return b
+"""
+
+# with args but not returning
 new_version = {
     "exec_count": 2,
     "__inputs": tuple(),
@@ -45,12 +35,6 @@ q1(-3, -1)
     "__outputs": tuple((6, -1)),
 }
 
-
-# with args but not returning
-# with args and returning
-# without args and returning
-
-
 code = """
 def q1(a, b):
     if a > b:
@@ -60,6 +44,47 @@ def q1(a, b):
         print(b)
         # return b
 """
+
+# with args and returning
+new_version = {
+    "exec_count": 2,
+    "__inputs": tuple(),
+    "__validate": """
+print(q1(5, 6))
+print(q1(-3, -1))
+""",
+    "__outputs": tuple((6, -1)),
+}
+
+code = """
+def q1(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+"""
+
+# without args and returning
+new_version = {
+    "exec_count": 2,
+    "__inputs": tuple(((5, 6), (-3, -1))),
+    "__validate": """
+print(q1())
+print(q1())
+""",
+    "__outputs": tuple((6, -1)),
+}
+
+code = """
+def q1():
+    a = int(input())
+    b = int(input())
+    if a > b:
+        return a
+    else:
+        return b
+"""
+
 
 res = requests.post(
     "http://127.0.0.1:5000/execute", json={"c": code, "tc": new_version}
